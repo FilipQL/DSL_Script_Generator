643f4bb0a000cdc101764cb1189d406d702f5fbf
Milos Zdravkovic <zmilos85@gmail.com>
2017-11-18 14:48:55 +0100

    Update config and initial topology

--- app.config.js
+++ app.config.js
@@ -8,58 +8,57 @@ var config = {
             "ams": "Amsterdam (ams)",
             "bra": "Bratislava (bra)",
             "ham": "Hamburg (ham)",
-            "lju": "Ljubljana (lju)",
             "lon": "London (lon)",
             "mil": "Milan (mil)",
-            "par": "Paris (par)",
-            "prg": "Prague (prg)"
+            "par": "Paris (par)" //,
+            //"prg": "Prague (prg)"
         },
 
         vsi: {
             "ams": "Amsterdam (ams)",
             "bra": "Bratislava (bra)",
             "ham": "Hamburg (ham)",
-            "lju": "Ljubljana (lju)",
             "lon": "London (lon)",
             "mil": "Milan (mil)",
-            "par": "Paris (par)",
-            "prg": "Prague (prg)"
+            "par": "Paris (par)" //,
+            //"prg": "Prague (prg)"
         },
 
         bms: {
             "ams": "Amsterdam (ams)",
             "bra": "Bratislava (bra)",
             "ham": "Hamburg (ham)",
-            "lju": "Ljubljana (lju)",
             "lon": "London (lon)",
             "mil": "Milan (mil)",
-            "par": "Paris (par)",
-            "prg": "Prague (prg)"
+            "par": "Paris (par)" //,
+            //"prg": "Prague (prg)"
         }
     },
 
     images: {
         host: [
-            "CentOS-6.8-x86_64-LiveDVD.iso",
-            "CentOS-7-x86_64-Minimal-1503-01.iso",
-            "Debian-8.6.0-amd64-DVD-1.iso",
-            "Kali-linux-2016.2-amd64.iso"
+            "Ubuntu-14.04.qcow2",
+            "ONOS-1.7.1.qcow2"
         ],
 
         bms: [
-            "CentOS-6.8-x86_64-LiveDVD.iso",
-            "CentOS-7-x86_64-Minimal-1503-01.iso",
-            "Debian-8.6.0-amd64-DVD-1.iso",
-            "Kali-linux-2016.2-amd64.iso"
+            "ubuntu-14.04-desktop.iso",
+            "ubuntu-14.04-server.iso",
+            "ubuntu-16.04-desktop.iso",
+            "ubuntu-16.04-server.iso",
+            "CentOS-6.x-LiveDVD.iso",
+            "CentOS-6.x-minimal.iso",
+            "CentOS-7-x86_64-DVD.iso",
+            "debian-live-x.y-amd64-gnome.iso",
+            "pclinuxos64-kde5.iso"
         ]
     },
 
     flavors: {
         host: [
-            "tiny",
-            "small",
-            "medium",
-            "large"
+            "c1r1h10",
+            "c2r2h20",
+            "c3r3h30"
         ],
 
         bms: [
@@ -69,22 +68,22 @@ var config = {
 
     switch_mode: [
         'hard',
-        'soft'
+       // 'soft'
     ],
 
     suggestions: {
-        host_id_prefix: "host",
+        host_id_prefix: "myVM",
 
-        vsi_id_prefix: "vsi",
+        vsi_id_prefix: "myVSI",
         suggested_switchIPv4Addr: "10.10.100.1",
         suggested_switchIPv4Mask: "255.255.255.0",
         suggested_controllerIPv4:"10.10.100.100",
         suggested_controllerPort: "6633",
 
-        bms_id_prefix: "bms"
+        bms_id_prefix: "myBMS"
     },
 
-    link_suffix: "_",
+    link_suffix: "num",
 
     bandwidths: {
         unit: "Mbps",
@@ -94,7 +93,7 @@ var config = {
         tooltip: "This field should be left unspecified since bandwidth reservation is not supported in this version of GTS."
     },
 
-    popover: false,
+    popover: true,
 
     vis_height: 0.7594,
     dsl_height: 0.706,
@@ -115,5 +114,5 @@ var config = {
         editClusterError: 'Clusters cannot be edited.'
     },
 
-    sampleData: {"dsl_id":"WorkshopTestbed","dsl_description":"Two BareMetalServers connected on a VSI which is controlled with one controller.","nodes":[{"id":1,"label":"host1","group":"host","dsl_id":"host1","location":"lon","image":"CentOS-6.8-x86_64-LiveDVD.iso","flavor":"tiny","ports":{"p1":"p1"},"ports_count":1,"selfieRadiuses":[],"x":0,"y":-160},{"id":2,"label":"vsi1","group":"vsi","dsl_id":"vsi1","location":"ams","switchIPv4Addr":"10.10.100.1","switchIPv4Mask":"255.255.2555.0","switchMode":"hard","controller":{"ipv4":"10.10.100.100","port":"6633"},"ports":{"p1":{"logicalPort":1},"p2":{"logicalPort":2},"pC":{"mode":"\"CONTROL\""}},"ports_count":2,"selfieRadiuses":[],"x":0,"y":50},{"id":3,"label":"bms1","group":"bms","dsl_id":"bms1","location":"ams","image":"CentOS-6.8-x86_64-LiveDVD.iso","ports":{"p1":"p1"},"ports_count":1,"selfieRadiuses":[],"x":-265,"y":145},{"id":4,"label":"bms2","group":"bms","dsl_id":"bms2","location":"lon","image":"CentOS-6.8-x86_64-LiveDVD.iso","ports":{"p1":"p1"},"ports_count":1,"selfieRadiuses":[],"x":265,"y":145}],"edges":[{"from":1,"to":2,"from_port":"p1","to_port":"pC","name":"host1vsi1_1","color":{"color":"#f94d51","highlight":"#952e30","hover":"#952e30"},"selfieRadius":false,"bandwidth":null,"id":"8bccbb1f-1656-4f24-8aa9-003d1c935a4d"},{"from":3,"to":2,"from_port":"p1","to_port":"p1","name":"bms1vsi1_1","selfieRadius":false,"bandwidth":null,"id":"68886928-f8e3-4eaf-ad62-d0770f4e6fe2","title":null},{"from":4,"to":2,"from_port":"p1","to_port":"p2","name":"bms2vsi1_1","selfieRadius":false,"bandwidth":null,"id":"253e8b47-8bb0-472c-907c-9259cf1625b3"}]}
+    sampleData:﻿{"dsl_id":"ExampleTestbed","dsl_description":"Two VMs and a link between them","nodes":[{"id":"2956013f-6956-4577-85ec-0df2b466fcfe","x":-240,"y":-37,"label":"Client","group":"host","dsl_id":"Client","location":"lon","image":"Ubuntu-14.04.qcow2","flavor":"c1r1h10","ports":{"p1":"p1"},"ports_count":1,"selfieRadiuses":[],"title":"<pre class=\"tooltip-pre\"><code>host {\n    id = \"Client\"\n    location = \"lon\"\n    imageId = \"Ubuntu-14.04.qcow2\"\n    flavorId = \"c1r1h10\n\tport { id = \"p1\" }\n}</code></pre>"},{"id":"8cde2bf2-afa6-4ace-89bc-03f2070bf973","x":150,"y":-37,"label":"Server","group":"host","dsl_id":"Server","location":"par","image":"Ubuntu-14.04.qcow2","flavor":"c1r1h10","ports":{"p1":"p1"},"ports_count":1,"selfieRadiuses":[],"title":"<pre class=\"tooltip-pre\"><code>host {\n    id = \"Server\"\n    location = \"par\"\n    imageId = \"Ubuntu-14.04.qcow2\"\n    flavorId = \"c1r1h10\n\tport { id = \"p1\" }\n}</code></pre>"}],"edges":[{"from":"2956013f-6956-4577-85ec-0df2b466fcfe","to":"8cde2bf2-afa6-4ace-89bc-03f2070bf973","name":"ClientServernum1","from_port":"p1","to_port":"p1","selfie":false,"id":"bc465025-9486-42a9-ab96-0d5d287aab56","title":"<pre class=\"tooltip-pre\"><code>link {\n    id = \"ClientServernum1\"\n    port { id = \"src\" }\n    port { id = \"dst\" }\n}\nadjacency Client.p1, ClientServernum1.src\nadjacency Server.p1, ClientServernum1.dst</code></pre>"}]}
 };
